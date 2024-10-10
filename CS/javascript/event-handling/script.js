d29d76711295a7a1336367677bbb2a0f465cbadf
LandonSims23 <landon.sims@hgs.hiddengeniusproject.org>
2024-10-09 17:38:30 -0700

    Update script.js October 9th 5:38

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,8 @@
 function eventHandling() {
     // Your code here.
 }
+//Find the button by its ID and use the addEventlistener()
+// method to make the button listen for button clicks
+document.getElementById("myButton").addEventListener("click",function(){
+    alert("Hello World")
+})
