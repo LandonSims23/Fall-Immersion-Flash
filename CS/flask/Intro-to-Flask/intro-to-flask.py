c618af5fd68417b77acb7bafc8cf0d341c05afe9
LandonSims23 <landon.sims@hgs.hiddengeniusproject.org>
2024-12-04 17:46:30 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,11 @@
-# instruction will below. 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Home Page!"
+
+
+if __name__ == '__main__':
+    app.run(debug = True)# instruction will below. 
